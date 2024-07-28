c0d164a972384030fe93d91482ded747f380af61
Dzhalal <djaldoit@gmai.com>
2024-07-28 18:03:32 +0300

    fix

--- config.py
+++ config.py
@@ -7,19 +7,12 @@ from utils import file
 def to_driver_options(context):
     options = UiAutomator2Options()
 
-    if context == 'emulator':
-        options.set_capability('remote_url', os.getenv('REMOTE_URL'))  # адрес удаленного сервера
-        options.set_capability('deviceName', os.getenv('DEVICE_NAME'))  # имя устройства
-        options.set_capability('appWaitActivity', os.getenv(
-            'APP_WAIT_ACTIVITY'))  # активити, которая будет открыта после запуска apk файла
-        # options.set_capability('udid', os.getenv('UDID')) # уникальный идентификатор устройства
-        options.set_capability('app', file.path_from_project(os.getenv('APP')))  # путь до apk файла
-
-    if context == 'real_device':
+    if context == 'real_device' or context == 'emulator':
         options.set_capability('remote_url', os.getenv('REMOTE_URL'))
         options.set_capability('deviceName', os.getenv('DEVICE_NAME'))
-        options.set_capability('appWaitActivity', os.getenv('APP_WAIT_ACTIVITY'))
-        # options.set_capability('udid', os.getenv('UDID'))
+        options.set_capability('appWaitActivity', os.getenv(
+            'APP_WAIT_ACTIVITY'))
+
         options.set_capability('app', file.path_from_project(os.getenv('APP')))
 
     if context == 'bstack':
@@ -30,7 +23,7 @@ def to_driver_options(context):
         options.set_capability('appWaitActivity', os.getenv('APP_WAIT_ACTIVITY'))
         options.set_capability('app', os.getenv('APP'))
         load_dotenv(dotenv_path=file.path_from_project(
-            '.env.bstack'))  # загрузка переменных окружения из файла .env.credentials
+            '.env.bstack'))
         options.set_capability(
             'bstack:options', {
                 'projectName': 'Wikipedia project',
